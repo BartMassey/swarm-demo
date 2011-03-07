af58720943b9c967b8c2e4663a939473d6a85b9e
Bart Massey <bart@cs.pdx.edu>
2011-03-06 17:17:29 -0800

    added license

--- Swarm.java
+++ Swarm.java
@@ -1,4 +1,10 @@
-// Copyright (c) 2011 Bart Massey
+/*
+ * Copyright (c) 2011 Bart Massey
+ * [This program is licensed under the "MIT License"]
+ * Please see the file COPYING in the source
+ * distribution of this software for license terms.
+ */
+
 
 // Animated "swarm of bugs" for AI swarm demo.
 
