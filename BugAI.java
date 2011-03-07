944bfc1aecadf97da40cea1fc56deed25187f247
Bart Massey <bart@cs.pdx.edu>
2011-03-07 13:48:46 -0800

    almost working bug controller

--- BugAI.java
+++ BugAI.java
@@ -18,7 +18,32 @@ public class BugAI extends AI {
     public void control(MeView me, AgentView[] agents, ThingView[] things) {
 	if (things != null)
 	    throw new Error("internal error: no things yet");
-	me.a = prng.nextDouble() * me.AMAX;
-	me.vt += Math.PI * nextSignedDouble() * dt;
+	me.a = 0;
+	me.vt = 0;
+	double cx = me.x - 0.5;
+	double cy = me.y - 0.5;
+	double ct = Math.PI / 2;
+	if (Math.abs(cx) <= 0.01) {
+	    if (Math.abs(cy) <= 0.01)
+		return;
+	    if (cy > 0)
+		cy = -Math.PI / 2;
+	} else {
+	    ct = Math.atan(-cy/cx);
+	}
+	double dt = ct - me.t;
+	System.err.println(dt);
+	if (Math.abs(dt) > 0.01) {
+	    if (dt > 0.01)
+		me.vt = me.VTMAX;
+	    else if (dt < -0.01)
+		me.vt = me.VTMIN;
+	    return;
+	}
+	double d = Math.sqrt(cx * cx + cy * cy);
+	if (-me.AMIN * me.v * me.v * 0.5 < d)
+	    me.a = -me.AMIN;
+	else
+	    me.a = me.AMIN;
     }
 }
