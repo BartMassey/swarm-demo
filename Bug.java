e7b5fb2c12f2dbbe6798099aa6647e78e44fd11a
Bart Massey <bart@cs.pdx.edu>
2012-11-30 14:52:07 -0800

    whitespace patch

--- Bug.java
+++ Bug.java
@@ -15,20 +15,20 @@ public class Bug extends Agent {
 
     /** Make a bug on the given playfield. */
     public Bug(Playfield playfield) {
-	super(playfield);
-	r = 0.02;
-	x = prng.nextDouble();
-	y = prng.nextDouble();
-	t = Math.PI * nextSignedDouble();
-	v = 0;
-	vt = 0;
-	a = 0;
-	AMIN = -0.5;
-	AMAX = 2.0;
-	VTMIN = - 2.0 * Math.PI;
-	VTMAX = 2.0 * Math.PI;
-	CDRAG = 100.0;
-	ai = new BugAI();
+        super(playfield);
+        r = 0.02;
+        x = prng.nextDouble();
+        y = prng.nextDouble();
+        t = Math.PI * nextSignedDouble();
+        v = 0;
+        vt = 0;
+        a = 0;
+        AMIN = -0.5;
+        AMAX = 2.0;
+        VTMIN = - 2.0 * Math.PI;
+        VTMAX = 2.0 * Math.PI;
+        CDRAG = 100.0;
+        ai = new BugAI();
     }
 
     /** Paint the bug on the screen.
@@ -37,20 +37,20 @@ public class Bug extends Agent {
      *  @param d   Scale parameter.
      */
     public void paintComponent(Graphics g, int d) {
-	g.setColor(Color.blue);
-	int x0 = (int) Math.floor(x * d + 0.5);
-	int y0 = (int) Math.floor(y * d + 0.5);
-	int r0 = (int) Math.floor(r * d + 0.5);
-	int x1 = x0 - (int) Math.floor(1.3 * r0 * Math.cos(t) + 0.5);
-	int y1 = y0 - (int) Math.floor(- 1.3 * r0 * Math.sin(t) + 0.5);
-	g.drawOval(x0 - r0, y0 - r0, 2 * r0, 2 * r0);
-	g.setColor(Color.red);
-	g.drawLine(x0, y0, x1, y1);
+        g.setColor(Color.blue);
+        int x0 = (int) Math.floor(x * d + 0.5);
+        int y0 = (int) Math.floor(y * d + 0.5);
+        int r0 = (int) Math.floor(r * d + 0.5);
+        int x1 = x0 - (int) Math.floor(1.3 * r0 * Math.cos(t) + 0.5);
+        int y1 = y0 - (int) Math.floor(- 1.3 * r0 * Math.sin(t) + 0.5);
+        g.drawOval(x0 - r0, y0 - r0, 2 * r0, 2 * r0);
+        g.setColor(Color.red);
+        g.drawLine(x0, y0, x1, y1);
     }
 
     /** When the bug collides, its velocity and acceleration
      *  go to zero. */
     public void thump() {
-	v = 0; a = 0;
+        v = 0; a = 0;
     }
 }
--- Swarm.java
+++ Swarm.java
@@ -16,32 +16,32 @@ import java.awt.*;
 public class Swarm {
     /** Run the demo. */
     public static void main(String[] args) {
-	if (args.length != 1) {
-	    System.err.println ("usage: Swarm <nbugs>");
-	    System.exit(1);
-	}
-	final int nbugs = Integer.parseInt(args[0]);
-	SwingUtilities.invokeLater(new Runnable() {
-		public void run() {
-		    createAndShowGUI(nbugs);
-		}
-	    });
+        if (args.length != 1) {
+            System.err.println ("usage: Swarm <nbugs>");
+            System.exit(1);
+        }
+        final int nbugs = Integer.parseInt(args[0]);
+        SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    createAndShowGUI(nbugs);
+                }
+            });
     }
 
     /** Set up the top-level window in which the GUI will run. */
     private static void createAndShowGUI(int nbugs) {
-	JFrame f = new JFrame("Swarm Demo");
-	f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	double dt = 0.01;
-	int ddt = 20;
-	if (nbugs > 20) {
-	    dt = 0.1;
-	    ddt = 2;
-	}
-	Playfield p = new Playfield(nbugs, dt, ddt);
+        JFrame f = new JFrame("Swarm Demo");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        double dt = 0.01;
+        int ddt = 20;
+        if (nbugs > 20) {
+            dt = 0.1;
+            ddt = 2;
+        }
+        Playfield p = new Playfield(nbugs, dt, ddt);
         f.add(p);
-	f.pack();
-	f.setVisible(true);
-	p.start();
+        f.pack();
+        f.setVisible(true);
+        p.start();
     }
 }
